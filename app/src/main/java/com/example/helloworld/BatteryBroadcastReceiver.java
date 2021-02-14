7aab571124a1e94e14ce150868a021a6d3a76825
Vishesh Goel <visheshgoelcs@gmail.com>
2021-02-14 18:41:31 +0530

    Update Toast Messages

--- app/src/main/java/com/example/helloworld/BatteryBroadcastReceiver.java
+++ app/src/main/java/com/example/helloworld/BatteryBroadcastReceiver.java
@@ -9,9 +9,21 @@ import android.widget.Toast;
 public class BatteryBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        StringBuilder sb = new StringBuilder();
+        /*StringBuilder sb = new StringBuilder();
         sb.append("Action: " + intent.getAction());
-        String log = sb.toString();
-        Toast.makeText(context, log, Toast.LENGTH_SHORT).show();
+        String log = sb.toString();*/
+        String message = "";
+        switch(intent.getAction()){
+            case Intent.ACTION_BATTERY_LOW:
+                message = "Action: "+ "ACTION_BATTERY_LOW";
+                break;
+            case Intent.ACTION_BATTERY_OKAY:
+                message = "Action: " + "ACTION_BATTERY_OKAY";
+                break;
+            case Intent.ACTION_POWER_DISCONNECTED:
+                message = "Action: " + "ACTION_POWER_DISCONNECTED";
+                break;
+        }
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
     }
 }
